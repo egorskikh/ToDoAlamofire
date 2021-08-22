fd185f0ab96a7230f0fc4753630a435dfa851970
Egor Gorskikh <egorgorskikh@gmail.com>
2021-08-22 22:01:16 +0300

    init: add project

---
+++ ToDoAlamofire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoAlamofire
+//
+//  Created by Egor Gorskikh on 22.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+    }
+
+
+}
+
