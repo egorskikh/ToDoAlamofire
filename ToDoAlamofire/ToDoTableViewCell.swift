c5d7b4e4476d3314ec6fe65291991570a4838290
Egor Gorskikh <egorgorskikh@gmail.com>
2021-08-23 11:39:54 +0300

    feat: setup storyboard

---
+++ ToDoAlamofire/ToDoTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ToDoTableViewCell.swift
+//  ToDoAlamofire
+//
+//  Created by Egor Gorskikh on 23.08.2021.
+//
+
+import UIKit
+
+class ToDoTableViewCell: UITableViewCell {
+
+    static let identifier = "ToDoCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
