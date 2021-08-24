7eab404dcd50f5e1a48667c2e3d088b1130f7f06
Egor Gorskikh <egorgorskikh@gmail.com>
2021-08-24 17:10:24 +0300

    feat: configure cell

--- ToDoAlamofire/ToDoModule/View/ToDoTableViewCell.swift
+++ ToDoAlamofire/ToDoModule/View/ToDoTableViewCell.swift
@@ -8,19 +8,19 @@
 import UIKit
 
 class ToDoTableViewCell: UITableViewCell {
-
+    
     // MARK: - Properties
     static let identifier = "ToDoCell"
-
+    
     // MARK: - IBOutlet
     @IBOutlet weak var taskLabel: UILabel!
     @IBOutlet weak var priorityСolor: UIView!
     @IBOutlet weak var prioritySymbolLabel: UILabel!
-
+    
     //MARK: - Public Method
     public func configureCell(todo: ToDo) {
         taskLabel.text = todo.item
-
+        
         switch todo.priority {
         case 0:
             priorityСolor.backgroundColor = .green
@@ -34,5 +34,6 @@ class ToDoTableViewCell: UITableViewCell {
         default:
             priorityСolor.backgroundColor = .white
             prioritySymbolLabel.text = "?"
+        }
     }
 }
