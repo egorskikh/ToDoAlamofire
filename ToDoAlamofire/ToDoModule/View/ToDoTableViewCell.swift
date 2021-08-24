6655232370066c920f756b383499009a204397c2
Egor Gorskikh <egorgorskikh@gmail.com>
2021-08-24 12:39:01 +0300

    feat: setup UI Cell

--- ToDoAlamofire/ToDoModule/View/ToDoTableViewCell.swift
+++ ToDoAlamofire/ToDoModule/View/ToDoTableViewCell.swift
@@ -9,17 +9,11 @@ import UIKit
 
 class ToDoTableViewCell: UITableViewCell {
 
+    // MARK: - Properties
     static let identifier = "ToDoCell"
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+    // MARK: - IBOutlet
+    @IBOutlet weak var taskLabel: UILabel!
+    @IBOutlet weak var priorityСolor: UIView!
 
 }
