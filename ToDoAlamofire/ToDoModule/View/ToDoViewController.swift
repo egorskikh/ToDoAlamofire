c19a483dda1487c12755a2375547380c9cf83eb4
Egor Gorskikh <egorgorskikh@gmail.com>
2021-08-24 13:41:12 +0300

    feat: setup UI TableView

--- ToDoAlamofire/ToDoModule/View/ToDoViewController.swift
+++ ToDoAlamofire/ToDoModule/View/ToDoViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ToDoViewController: UIViewController {
 
+    // MARK: - IBOutlet
+    @IBOutlet weak var tasksTableView: UITableView!
+
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureView()
+        configureNavigationController()
+    }
+
+    //  MARK: - Private Method
+    private func configureView() {
         view.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
     }
 
+    private func configureNavigationController() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    // then delete
+    private func testAlamofire() {
+
+    }
 
 }
 
+// MARK: - UITableViewDataSource
+extension ToDoViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tasksTableView.dequeueReusableCell(withIdentifier: ToDoTableViewCell.identifier, for: indexPath)
+        as! ToDoTableViewCell
+        cell.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.7249955512, blue: 0.7472946008, alpha: 1)
+        return cell
+    }
+
+}
+
+// MARK: - UITableViewDelegate
+extension ToDoViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+           return 109
+       }
+
+}
