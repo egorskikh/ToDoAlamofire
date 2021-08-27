c4ff297d30701f6a94517fc5ba05db7f5e1fb8ca
Egor Gorskikh <egorgorskikh@gmail.com>
2021-08-27 20:33:17 +0300

    ref: del extra

--- ToDoAlamofire/ToDoModule/Contracts/ToDoContracts.swift
+++ ToDoAlamofire/ToDoModule/Contracts/ToDoContracts.swift
@@ -11,6 +11,7 @@ import UIKit
 
 protocol ViewProtocol: AnyObject {
     var presenter: PresenterProtocol? { get set }
+    
     func showTodos(_ todos: [ToDo])
 }
 
--- ToDoAlamofire/ToDoModule/Service/ToDoNetworking.swift
+++ ToDoAlamofire/ToDoModule/Service/ToDoNetworking.swift
@@ -12,17 +12,15 @@ class ToDoNetworking {
     typealias DataCompletionHandler = ([ToDo]) -> Void
 
     func getResponse(completionHandler: @escaping DataCompletionHandler ) {
-        var returnArr: [ToDo] = []
-        let request = Session.default.request("http://localhost:3003/")
 
+        let request = Session.default.request("http://localhost:3003/")
         request.responseDecodable(of: ToDos.self) { (response) in
 
             switch response.result {
-
+            
             case .success(let tasks):
-                print("SUCCESS to FETCH JSON: \(tasks)")
-                returnArr = tasks.items
-                completionHandler(returnArr)
+                print("SUCCESS to FETCH JSON")
+                completionHandler(tasks.items)
             case .failure(let error):
                 print("FAILED to FETCH JSON: \(error.localizedDescription)")
             }
